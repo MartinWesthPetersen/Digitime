3cb4989536cf45d3e9ec6b15f4523aef4102e353
Martin Westh Petersen <martin_westh@hotmail.com>
2013-04-09 16:24:27 +0200

    DIGITIME

--- DIGITIME/src/Main.java
+++ DIGITIME/src/Main.java
@@ -5,6 +5,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		MainFrame.instance.setVisible(true);
-		System.out.println("kjlJ");
+		System.out.println("kjasasdasdasdasdlJ");
 	}
 }
